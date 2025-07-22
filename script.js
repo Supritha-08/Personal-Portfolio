8af8b0269809ddd2340029041f8c51aef00d69dc
supritha-08 <patilsupritha9@gmail.com>
2025-07-23 03:59:21 +0530

    Projects added

--- script.js
+++ script.js
@@ -3,3 +3,28 @@ const hamburger=document.querySelector('.hamburger');
         hamburger.addEventListener('click',()=>{
             menu_bar.classList.toggle('show');
         });
+
+  const swiper = new Swiper(".mySwiper", {
+    loop: true,
+    autoplay: {
+      delay: 2000,
+    },
+    slidesPerView: 1,
+    spaceBetween: 20,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      768: { slidesPerView: 2 },
+      1024: { slidesPerView: 3 }
+    }
+  });
+
+
+
+
