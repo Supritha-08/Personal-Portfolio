3e71982c2ac66577f8ce21cc440a8605e8270e88
supritha-08 <patilsupritha9@gmail.com>
2025-07-27 03:29:52 +0530

    updating

--- script.js
+++ script.js
@@ -17,3 +17,30 @@ const observer = new IntersectionObserver(([entry]) => {
 }, { threshold: 0.5 });
 
 observer.observe(sec1);
+
+
+
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      1024: { slidesPerView: 3 },
+      768: { slidesPerView: 2 },
+      480: { slidesPerView: 1 },
+    }
+  });
+
+
