121e2489324a7982a7bb55241abb36ef8ea4b151
supritha-08 <patilsupritha9@gmail.com>
2025-07-27 01:09:10 +0530

    updating

--- script.js
+++ script.js
@@ -1,43 +1,19 @@
-const hamburger=document.querySelector('.hamburger');
-        const menu_bar=document.querySelector('.navbar-menu .menu-bar');
-        hamburger.addEventListener('click',()=>{
-            menu_bar.classList.toggle('show');
-        });
-        const navbar = document.querySelector('.navbar');
-  const sec1 = document.querySelector('#sec1');
+const hamburger = document.querySelector('.hamburger');
+const menu_bar = document.querySelector('.menu-bar'); // updated selector
 
-  const observer = new IntersectionObserver(([entry]) => {
-    if (entry.isIntersecting) {
-      navbar.style.display = 'flex';
-    } else {
-      navbar.style.display = 'none';
-    }
-  }, { threshold: 0.5 });
+hamburger.addEventListener('click', () => {
+  menu_bar.classList.toggle('show');
+});
+
+const navbar = document.querySelector('.navbar');
+const sec1 = document.querySelector('#sec1');
+
+const observer = new IntersectionObserver(([entry]) => {
+  if (entry.isIntersecting) {
+    navbar.style.display = 'flex';
+  } else {
+    navbar.style.display = 'none';
+  }
+}, { threshold: 0.5 });
 
 observer.observe(sec1);
-
-
-  const swiper = new Swiper(".mySwiper", {
-    loop: true,
-    autoplay: {
-      delay: 2000,
-    },
-    slidesPerView: 1,
-    spaceBetween: 20,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    breakpoints: {
-      768: { slidesPerView: 2 },
-      1024: { slidesPerView: 3 }
-    }
-  });
-
-
-
-
